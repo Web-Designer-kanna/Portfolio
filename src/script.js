2dd3234689f87c1633125891726524aa20099210
kannan <developerkanna045@gmail.com>
2024-02-20 17:04:56 -0800

    feature added

--- src/script.js
+++ src/script.js
@@ -2,9 +2,10 @@ const toggleMode=document.querySelector(".mode");
 const menuToggle=document.querySelector("header nav .fa-bars");
 const ul=document.querySelector("header nav ul");
 const closeBtn=document.querySelector("header nav ul .fa-close");
+const Body=document.querySelector("body");
 
 toggleMode.addEventListener("click",()=>{
-    document.querySelector("body").classList.toggle("active");
+    Body.classList.toggle("active");
 });
 menuToggle.addEventListener("click",()=>{
     ul.classList.toggle("active");
